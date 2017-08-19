2360d513f92ae072e64d66e81c0e080a9d9ebd5e
JuliusDorfman <juliusdorfman@gmail.com>
2017-08-18 20:44:09 -0700

    FINAL PRODUCT

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,24 @@
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var arrayWordsToGuess = ['dog', 'cat', 'bird', 'turtle', 'crocodile', 'alligator', 'giraffe', 'snake', 'platypus', 'hippopotamus']
-var arrayLettersGuessed = [];
 var game = {};
-// var random = Math.floor(Math.random() * arrayWordsToGuess.length) + 1;
-var firstRun = true;
-
 init();
 var numWrong = 0;
 var guessesRemaining = 10;
+var winCounter = 0;
+var lossCounter = 0;
 
 function init() {
+    game = {};
     console.log('', "Game is running");
     setRandomLetter();
     document.onkeyup = onUserInput;
-    // document.onkeyup = userLettersGuessed;
+    resetlistLettersGuessed();
+
 }
 
 function onUserInput(e) {
     var userInput = e.key;
     if (alphabet.indexOf(userInput) === -1) {
-        console.log('invalid input', 'please use a letter');
+        console.log('', 'Please choose a letter');
         return;
     }
 
@@ -27,22 +26,30 @@ function onUserInput(e) {
     userChoice.innerHTML = userInput;
 
     if (userInput === game.letter) {
-        alert("Good Job");
+        alert("Yer a Wizerd, 'Arry!");
         resetWrongCounter();
         resetGuessesRemaining();
-        firstRun = false;
+        funWinCounter();
         init();
     } else {
         wrongCounter();
         wrongGuesses();
-        if (guessesRemaining == 0) {
-            alert('Go back to Hogwarts');
-            resetWrongCounter();
-            resetGuessesRemaining();
-            init();
-        }
     }
+    if (guessesRemaining > 0) {
+        listLettersGuessed(userInput);
+    }
+
+    if (guessesRemaining == 0) {
+        alert('Go back to Hogwarts');
+        resetWrongCounter();
+        resetGuessesRemaining();
+        funLossCounter();
+        resetlistLettersGuessed();
+        init();
+    }
+
 }
+
 
 function setRandomLetter() {
     var randNum = Math.random() * 26;
@@ -75,22 +82,50 @@ function wrongGuesses() {
 
 function resetGuessesRemaining() {
     guessesRemaining = 10;
-    console.log('guesses-remaining set to', guessesRemaining);
+    console.log('guesses-remaining set to:', guessesRemaining);
     document.getElementById('guesses-remaining').innerHTML = guessesRemaining;
 }
 
+function listLettersGuessed(userInput) {
+    console.log('', userInput);
+    game.listUserInput = (game.listUserInput || '') + userInput + (', ' || ' ');
+    document.getElementById('letters-guessed').innerHTML = game.listUserInput;
+}
 
+function resetlistLettersGuessed() {
+    game.listLettersGuessed = " ";
+    document.getElementById('letters-guessed').innerHTML = game.listLettersGuessed;
+}
 // please explain in english what is happening *scope*? if I put "game." and/or "var" before my numWong and guessesRemaining variables
 
+function funWinCounter() {
+    winCounter += 1;
+    document.getElementById('win-number').innerHTML = winCounter;
+}
+
+function funLossCounter() {
+    lossCounter += 1;
+    document.getElementById('loss-number').innerHTML = lossCounter;
+}
+
+// UNUSED HANG-MAN CODE:
 
 // function listLettersGuessed(e) {
 //     var userLettersGuessed = e.key;
 //     document.getElementById('letters-guessed').innerHTML = userLettersGuessed;
-//     document.write = userLettersGuessed;
-        // if (userLettersGuessed!===arrayLettersGuessed) {
-//             document.write =userLettersGuessed;
-//         }
-// // }
+// if (userLettersGuessed! === arrayLettersGuessed) {
+// //             document.write =userLettersGuessed;
+// //         }
+// // // }
+// function checkLetters(event)
+// {
+//   var o = {};
+//   for(var i=0;i<a.length;i++)
+//   {
+//     o[a[i]]='';
+//   }
+//   return o;
+// }
 
 
 
@@ -101,3 +136,11 @@ function resetGuessesRemaining() {
 
 // chooseWord();
 
+
+// var arrayWordsToGuess = ['dog', 'cat', 'bird', 'turtle', 'crocodile', 'alligator', 'giraffe', 'snake', 'platypus', 'hippopotamus']
+// var random = Math.floor(Math.random() * arrayWordsToGuess.length) + 1;
+//
+
+// function palindrome(word) {
+//     return (word == word.split('').reverse().join(''));
+// }
